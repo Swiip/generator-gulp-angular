8bc12f7681ff2f26a08ce11776fed6df10eab719
Matthieu Lux <matthieu.lux@gmail.com>
2014-08-22 11:26:18 +0200

    remove bad copy paste

--- app/templates/gulp/__less.js
+++ app/templates/gulp/__less.js
@@ -1,7 +1,7 @@
 gulp.task('styles', function () {
   return gulp.src('app/styles/*.less')
     .pipe($.plumber())
-    .pipe($.less({style: 'expanded'}))
+    .pipe($.less())
     .pipe($.autoprefixer('last 1 version'))
     .pipe(gulp.dest('.tmp/styles'))
     .pipe($.size());
