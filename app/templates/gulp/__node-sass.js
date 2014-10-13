57af86e48b366bc4eafb48d1039675d378194eaa
zckrs <mdara@eleven-labs.com>
2014-10-13 18:41:08 +0200

    Task styles depend on wiredep

--- app/templates/gulp/__node-sass.js
+++ app/templates/gulp/__node-sass.js
@@ -1,4 +1,4 @@
-gulp.task('styles', function () {
+gulp.task('styles', ['wiredep'],  function () {
   return gulp.src('src/{app,components}/**/*.scss')
     .pipe($.sass({style: 'expanded'}))
     .on('error', handleError)
--- app/templates/gulp/_watch.js
+++ app/templates/gulp/_watch.js
@@ -2,7 +2,7 @@
 
 var gulp = require('gulp');
 
-gulp.task('watch', ['wiredep'<% if(stylesBuild) { %>, 'styles'<% } %>] ,function () {
+gulp.task('watch', [<% if(stylesBuild) { %>'styles'<% } %>] ,function () {
 <% if(stylesBuild) { %>  gulp.watch('src/{app,components}/**/*.<%= styleExtension %>', ['styles']);
 <% } %>  gulp.watch('src/{app,components}/**/*.js', ['scripts']);
   gulp.watch('src/assets/images/**/*', ['images']);
