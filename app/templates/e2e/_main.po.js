8ade931f55eb36586e45820c1acc30626196663d
TMD <dung1@e.ntu.edu.sg>
2014-12-07 19:10:06 +0800

    Fix e2e tests #131.

--- app/templates/e2e/_main.po.js
+++ app/templates/e2e/_main.po.js
@@ -6,13 +6,14 @@
 'use strict';
 
 var MainPage = function() {
+<% if (props.ui.key === 'foundation') { %>
+  this.jumbEl = element(by.css('.panel'));
+<% } else { %> 
   this.jumbEl = element(by.css('.jumbotron'));
+<% } %>
   this.h1El = this.jumbEl.element(by.css('h1'));
   this.imgEl = this.jumbEl.element(by.css('img'));
-
-  this.techEl = element(by.css('.row'));
-  this.thumbnailEls = this.techEl.all(by.repeater('awesomeThing in awesomeThings'));
+  this.thumbnailEls = element(by.css('body')).all(by.repeater('awesomeThing in awesomeThings'));
 };
 
 module.exports = new MainPage();
-
