fdc635e4a2140125c02038f25367cf369e8d6704
Andy Perlitch <andyperlitch@gmail.com>
2015-02-11 11:21:57 -0800

    Sorts source modules in unit tests, fixes #188

--- app/templates/gulp/_unit-tests.js
+++ app/templates/gulp/_unit-tests.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 
 var $ = require('gulp-load-plugins')();
 
+var merge = require('merge-stream');
 var wiredep = require('wiredep');
 
 var paths = gulp.paths;
@@ -15,9 +16,8 @@ function runTests (singleRun) {
     dependencies: true,
     devDependencies: true
   });
-
-  var testFiles = bowerDeps.js.concat([
-<% if (props.jsPreprocessor.key === 'none') { %>
+  var testFiles = gulp.src(bowerDeps.js);
+  var srcFiles = gulp.src([ <% if (props.jsPreprocessor.key === 'none') { %>
     paths.src + '/{app,components}/**/*.js'
 <% } else if (props.jsPreprocessor.extension === 'js') { %>
     paths.tmp + '/serve/app/index.js',
@@ -33,9 +33,9 @@ function runTests (singleRun) {
     paths.src + '/{app,components}/**/*.spec.js',
     paths.src + '/{app,components}/**/*.mock.js'
 <% } %>
-  ]);
+  ]).pipe($.angularFilesort());
 
-  return gulp.src(testFiles)
+  return merge(testFiles, srcFiles)
     .pipe($.karma({
       configFile: 'karma.conf.js',
       action: (singleRun)? 'run': 'watch'
