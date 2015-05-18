b02aa686279d76735ca3f059a6a7a50d7a6173f5
Mehdy Dara <mdara@eleven-labs.com>
2015-05-18 14:29:06 +0200

    Remove scope in main.controller.spec.js

--- app/templates/src/app/main/_main.controller.spec.js
+++ app/templates/src/app/main/_main.controller.spec.js
@@ -2,13 +2,8 @@
   'use strict';
 
   describe('controllers', function(){
-    var scope;
 
     beforeEach(module('<%= appName %>'));
-
-    beforeEach(inject(function($rootScope) {
-      scope = $rootScope.$new();
-    }));
 
     it('should define more than 5 awesome things', inject(function($controller) {
       var vm = $controller('MainController');
