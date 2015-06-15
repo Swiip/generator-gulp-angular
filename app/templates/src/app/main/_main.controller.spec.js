1f5be42ee3fcd0bfe9e3c2f64df1d058eb5b87b3
Mehdy Dara <mdara@eleven-labs.com>
2015-06-15 14:42:35 +0200

    Complete unit test for main.controller.js

--- app/templates/src/app/main/_main.controller.spec.js
+++ app/templates/src/app/main/_main.controller.spec.js
@@ -2,14 +2,38 @@
   'use strict';
 
   describe('controllers', function(){
+    var vm;
+    var $timeout;
+    var toastr;
 
     beforeEach(module('<%- appName %>'));
+    beforeEach(inject(function(_$controller_, _$timeout_, _webDevTec_, _toastr_) {
+      spyOn(_webDevTec_, 'getTec').and.returnValue(['There', 'are', 'five', 'awesome', 'technos']);
+      spyOn(_toastr_, 'info').and.callThrough();
 
-    it('should define more than 5 awesome things', inject(function($controller) {
-      var vm = $controller('MainController');
-
-      expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-      expect(vm.awesomeThings.length > 5).toBeTruthy();
+      vm = _$controller_('MainController');
+      $timeout = _$timeout_;
+      toastr = _toastr_;
     }));
+
+    it('should have a timestamp creation date', function() {
+      expect(vm.creationDate).toEqual(jasmine.any(Number));
+    });
+
+    it('should define animate class after delaying timeout ', function() {
+      $timeout.flush();
+      expect(vm.classAnimation).toEqual('rubberBand');
+    });
+
+    it('should show a Toastr info and stop animation when invoke showToastr()', function() {
+      vm.showToastr();
+      expect(toastr.info).toHaveBeenCalled();
+      expect(vm.classAnimation).toEqual('');
+    });
+
+    it('should define more than 5 awesome things', function() {
+      expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
+      expect(vm.awesomeThings.length === 5).toBeTruthy();
+    });
   });
 })();
