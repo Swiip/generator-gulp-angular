f05b4f2288237694e4ac194f32cb07d7688576c3
Mehdy Dara <mdara@eleven-labs.com>
2015-06-24 11:20:53 +0200

    Add test for malarkey directive

---
+++ app/templates/src/app/components/malarkey/_malarkey.directive.spec.js
@@ -0,0 +1,48 @@
+(function() {
+  'use strict';
+
+  /**
+   * @todo Complete the test
+   * This example is not perfect.
+   * The `link` function is not tested.
+   * (malarkey usage, addClass, $watch, $destroy)
+   */
+  describe('directive malarkey', function() {
+    var $log;
+    var vm;
+    var el;
+
+    beforeEach(module('<%- appName %>'));
+    beforeEach(inject(function($compile, $rootScope, githubContributor, $q, _$log_) {
+      $log = _$log_;
+
+      spyOn(githubContributor, 'getContributors').and.callFake(function() {
+        var deferred = $q.defer();
+        deferred.resolve([{}, {}, {}, {}, {}, {}]);
+
+        return deferred.promise;
+      });
+
+      el = angular.element('<acme-malarkey extra-values="[\'Poney\', \'Monkey\']"></acme-malarkey>');
+
+      $compile(el)($rootScope.$new());
+      $rootScope.$digest();
+      vm = el.isolateScope().vm;
+    }));
+
+    it('should be compiled', function() {
+      expect(el.html()).not.toEqual(null);
+    });
+
+    it('should have isolate scope object with instanciate members', function() {
+      expect(vm).toEqual(jasmine.any(Object));
+
+      expect(vm.contributors).toEqual(jasmine.any(Array));
+      expect(vm.contributors.length).toEqual(6);
+    });
+
+    it('should log a info', function() {
+      expect($log.info.logs).toEqual(jasmine.stringMatching('Activated Contributors View'));
+    });
+  });
+})();
