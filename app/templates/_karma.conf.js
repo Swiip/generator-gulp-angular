330d7507a5df6d75cfec08b8997832686b9d4ccd
Mehdy Dara <mdara@eleven-labs.com>
2015-06-24 14:58:34 +0200

    Add sample ES6 unit test

--- app/templates/_karma.conf.js
+++ app/templates/_karma.conf.js
@@ -26,6 +26,8 @@ function listFiles() {
       path.join(conf.paths.tmp, '/**/*.mock.js'),
 <% } else { -%>
       path.join(conf.paths.tmp, '/serve/app/index.module.js'),
+      path.join(conf.paths.src, '/**/*.spec.js'),
+      path.join(conf.paths.src, '/**/*.mock.js'),
 <% } -%>
       path.join(conf.paths.src, '/**/*.html')
     ]);
