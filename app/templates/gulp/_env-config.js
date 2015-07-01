282ec3eeb2c177a23d4dc0fd5968a0084d9bc488
Chris Hale <chrishale@CHRHALE-M-D0WK.(none)>
2015-07-01 16:01:59 -0500

    Added ability for Gulp workflow to inject constants into app module based on command line --env variable .  Useful for angular projects that have different app api keys based on env or different rest endpoints based on deployed environment

---
+++ app/templates/gulp/_env-config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var fs = require('fs');
+var argv = require('yargs').argv;
+var $ = require('gulp-load-plugins')();
+
+//var ngConstant = require('gulp-ng-constant');
+
+var environment = argv.env || 'default';
+//var ENV = JSON.parse(fs.readFileSync('./src/app/env_configs/config-' + environment + '.json', 'utf8')).ENV;
+
+gulp.task('env-config', function () {
+  gulp.src('src/app/env_configs/config-' + environment + '.json')
+    .pipe($.ngConstant({
+      name: '<%- appName %>',
+      deps: false,
+      dest: 'constants.config.js'
+    }))
+    .pipe(gulp.dest('src/app'));
+});
