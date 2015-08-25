036e1a7e608892885337daaf04c099f080336aa0
Matthieu Lux <matthieu.lux@gmail.com>
2015-08-25 09:58:57 +0200

    add missing --dev option to shrinkwrap

--- scripts/update-test-shrinkwrap.sh
+++ scripts/update-test-shrinkwrap.sh
@@ -4,14 +4,12 @@ set -e
 
 rm -rf test/tmp/deps/node_modules test/tmp/deps/package.json test/tmp/deps/npm-shrinkwrap.json
 
-node test/template-cli.js prepare
-
 node test/template-cli.js deps
 
 cd test/tmp/deps
 
 npm install
 
-npm shrinkwrap
+npm shrinkwrap --dev
 
 cp ./npm-shrinkwrap.json ../../npm-shrinkwrap.json
