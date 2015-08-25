c21ad4cb279d808fd08cd33b583a7605ac9f1a3b
Matthieu Lux <matthieu.lux@gmail.com>
2015-08-25 09:21:15 +0200

    add a script to update test shrinkwrap

---
+++ scripts/update-test-shrinkwrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+rm -rf test/tmp/deps/node_modules test/tmp/deps/package.json test/tmp/deps/npm-shrinkwrap.json
+
+node test/template-cli.js prepare
+
+node test/template-cli.js deps
+
+cd test/tmp/deps
+
+npm install
+
+npm shrinkwrap
+
+cp ./npm-shrinkwrap.json ../../npm-shrinkwrap.json
