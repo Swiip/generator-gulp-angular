027332df60e4683e9a0abd03c86bac7a89cad199
Mehdy Dara <mdara@eleven-labs.com>
2015-09-01 18:26:44 +0200

    Rewrite main.spec.ts in TypeScript

--- app/templates/src/app/main/_main.controller.spec.ts
+++ app/templates/src/app/main/_main.controller.spec.ts
@@ -1,18 +1,14 @@
-(function() {
-  'use strict';
+/**
+ * @todo Write test in TypeScript
+ */
+describe('controllers', () => {
 
-  /**
-   * @todo Write test in TypeScript
-   */
-  describe('controllers', function(){
+  beforeEach(angular.mock.module('<%- appName %>'));
 
-    beforeEach(angular.mock.module('<%- appName %>'));
+  it('should define more than 5 awesome things', inject(($controller: ng.IControllerService) => {
+    var vm = $controller('MainController');
 
-    it('should define more than 5 awesome things', inject(function($controller) {
-      var vm = $controller('MainController');
-
-      expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-      expect(vm.awesomeThings.length > 5).toBeTruthy();
-    }));
-  });
-})();
+    expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
+    expect(vm.awesomeThings.length > 5).toBeTruthy();
+  }));
+});
